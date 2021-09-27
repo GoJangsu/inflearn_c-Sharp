1669e1ff959521233b084be7a0c2fdf8deef34f6
GoJangsu <82722100+GoJangsu@users.noreply.github.com>
2021-09-27 21:58:38 +0900

    상속
    c# 상속 기초 공부

---
+++ inflearn_c Sharp9(상속)/inflearn_c Sharp9(상속)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inflearn_test9_상속_
+{
+    //부모 클래스(parentsClass)
+    class parentsClass
+    {
+        protected string year = "2021년\n\n";
+
+        public void study()
+        {
+            Console.WriteLine("C# 공부를 합니다.\n");
+        }
+    }
+
+    //자식 클래스(childClass)가 부모클래스(parentsClass)를 상속받음
+    class childClass : parentsClass
+    {
+        public string Start()
+        {
+            return year;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            childClass Inheritance = new childClass();
+            Inheritance.study();
+
+            Console.WriteLine(Inheritance.Start());
+
+            /*
+             오류발생
+             Inheritance.a;
+             */
+        }
+
+    }
+}
